c97c41724e25052a86c39a388a92f5dbcbc50caf
Rômullo Cordeiro Rodrigues <45169227+xXDeonoXx@users.noreply.github.com>
2021-10-31 12:06:08 -0300

    feat: finish category crud

--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -1,26 +1,77 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Page } from 'src/shared/Page';
+import { createQueryBuilder, Repository } from 'typeorm';
 import { CreateCategoryDto } from './dto/create-category.dto';
+import { FindCategoryParams } from './dto/find-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { Category } from './entities/category.entity';
 
 @Injectable()
 export class CategoriesService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(
+    @InjectRepository(Category)
+    private categoryRepository: Repository<Category>
+  ) {}
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    const { name } = createCategoryDto;
+
+    const category = await this.categoryRepository.findOne({ where: { name } });
+
+    if (category)
+      throw new HttpException(
+        'This category already exists',
+        HttpStatus.FORBIDDEN
+      );
+
+    return await this.categoryRepository.save({ name });
   }
 
-  findAll() {
-    return `This action returns all categories`;
+  async findPage(pageable: FindCategoryParams): Promise<Page<Category>> {
+    const { size = 10, page: pageNumber = 1, name } = pageable;
+    const query = createQueryBuilder(Category, 'category');
+
+    if (name) {
+      query.andWhere('lower(category.name) like :name', {
+        name: `%${name
+          .toLowerCase()
+          .trim()
+          .replace(/[^\w\s]/gi, '')}%`,
+      });
+    }
+
+    query.take(Number(size));
+    if (pageNumber === 1) {
+      query.skip(Number(pageNumber) - 1);
+    } else {
+      const pageNumberMinusone = Number(pageNumber) - 1;
+      query.skip(Number(size) * Number(pageNumberMinusone));
+    }
+    const [categories, count] = await query.getManyAndCount();
+    return new Page(categories, count, pageNumber, size);
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category)
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+    return category;
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category)
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+
+    return this.categoryRepository.save({ ...category, ...updateCategoryDto });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    const category = await this.categoryRepository.findOne({ where: { id } });
+    if (!category)
+      throw new HttpException('Category not found', HttpStatus.NOT_FOUND);
+
+    return await this.categoryRepository.remove(category);
   }
 }
