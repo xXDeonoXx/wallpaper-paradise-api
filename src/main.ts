2f8df4218aeff3e79dc52a2176dd20f5a8878a02
Rômullo Cordeiro <romullocordeiro@gmail.com>
2022-01-10 02:25:12 -0300

    feat: use cors

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { config } from 'aws-sdk';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   const configService = app.get(ConfigService);
   config.update({
     accessKeyId: configService.get('AWS_ACCESS_KEY_ID'),
